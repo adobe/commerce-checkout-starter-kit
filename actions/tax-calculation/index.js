cbb106d8ddd1b95a4d6a10fcca778b8485fea265
Mohsin Zaman <80530839+MohsinZamanShaheen@users.noreply.github.com>
2025-04-22 13:05:52 +0200

    fix linting errors

--- actions/tax-calculation/index.js
+++ actions/tax-calculation/index.js
@@ -119,8 +119,8 @@ function createTaxBreakdownOperation(index, tax, taxAmount) {
         amount: taxAmount,
         title: tax.title,
         tax_rate_key: `${tax.code}-${tax.rate}`,
-      }
-    }
+      },
+    },
   };
 }
 
@@ -133,8 +133,8 @@ function createTaxSummaryOperation(index, itemTaxRate, itemTaxAmount, discountCo
         rate: itemTaxRate,
         amount: itemTaxAmount,
         discount_compensation_amount: discountCompensationTaxAmount,
-      }
-    }
+      },
+    },
   };
 }
 
