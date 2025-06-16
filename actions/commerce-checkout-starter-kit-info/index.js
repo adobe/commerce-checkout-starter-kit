badd51be00557eb94f28e5f65ede70a4d1960f65
Oriol Barcelona <obarcelonapa@adobe.com>
2025-06-16 17:24:02 +0200

    CEXT-4812: fix warnings (#36)

--- actions/commerce-checkout-starter-kit-info/index.js
+++ actions/commerce-checkout-starter-kit-info/index.js
@@ -15,6 +15,8 @@ const { HTTP_OK } = require('../../lib/http');
 /**
  * Please DO NOT DELETE this action; future functionalities planned for upcoming starter kit releases may stop working.
  * This is an info endpoint which is used to the track adoption of the starter kit.
+ * @param {object} params action input parameters.
+ * @returns {object} returns a response object
  */
 function main(params) {
   return { statusCode: HTTP_OK };
