d1d9cfb8e8114dae9aa28381e56f02224c04558e
Oriol Barcelona <obarcelonapa@adobe.com>
2025-06-19 14:05:33 +0200

    Get IMS context Oauth s2s from project

--- scripts/sync-oauth-credentials.js
+++ scripts/sync-oauth-credentials.js
@@ -2,33 +2,36 @@ const fs = require('fs');
 const { Core } = require('@adobe/aio-sdk');
 const { replaceEnvVar, resolveEnvPath } = require('../lib/env');
 const dotenv = require('dotenv');
+const { context } = require('@adobe/aio-lib-ims');
 
 const keyMap = {
-  client__id: 'OAUTH_CLIENT_ID',
-  client__secrets: 'OAUTH_CLIENT_SECRETS',
-  technical__account__email: 'OAUTH_TECHNICAL_ACCOUNT_EMAIL',
-  technical__account__id: 'OAUTH_TECHNICAL_ACCOUNT_ID',
+  client_id: 'OAUTH_CLIENT_ID',
+  client_secrets: 'OAUTH_CLIENT_SECRETS',
+  technical_account_email: 'OAUTH_TECHNICAL_ACCOUNT_EMAIL',
+  technical_account_id: 'OAUTH_TECHNICAL_ACCOUNT_ID',
   scopes: 'OAUTH_SCOPES',
-  ims__org__id: 'OAUTH_IMS_ORG_ID',
+  ims_org_id: 'OAUTH_IMS_ORG_ID',
 };
 
 const logger = Core.Logger('scripts/sync-oauth-credentials', { level: process.env.LOG_LEVEL || 'info' });
 
 /**
- * Syncs OAUTH environment variables from AIO_ims_contexts_* variables in the .env file.
+ * Syncs OAUTH environment variables from the configured IMS context in the .env file.
  */
-function main() {
+async function main() {
   try {
-    logger.debug('Sync OAUTH env vars from AIO_ims_contexts_* vars in .env file');
+    logger.debug('Sync OAUTH env vars from configured IMS context in .env file');
     const envPath = resolveEnvPath();
     const envVars = dotenv.parse(fs.readFileSync(envPath, 'utf8'));
-    const imsContextEnvVars = parseImsContextEnvVars(envVars);
-    if (imsContextEnvVars.length === 0) {
-      logger.warn('No AIO_ims_contexts_* environment variables found in .env file');
+    const imsContext = await resolveImsS2SContext();
+    if (!imsContext) {
+      logger.warn('IMS server 2 server OAuth credentials are not configured. Did you run `aio app use`?');
       return;
     }
 
-    imsContextEnvVars.forEach(({ credential, key, value }) => {
+    const { name: credential, data } = imsContext;
+
+    Object.entries(data).forEach(([key, value]) => {
       const oauthKey = keyMap[key];
       if (!oauthKey) {
         logger.warn(`No mapping found for key: ${key}`);
@@ -37,12 +40,12 @@ function main() {
 
       if (!envVars[oauthKey]) {
         replaceEnvVar(envPath, oauthKey, value);
-        logger.info(`Added ${oauthKey} with value from AIO_ims_contexts_${credential}_${key}`);
+        logger.info(`Added ${oauthKey} with value from ${key} of IMS context ${credential}`);
       } else if (envVars[oauthKey] !== value) {
         replaceEnvVar(envPath, oauthKey, value);
-        logger.info(`Replaced ${oauthKey} with value from AIO_ims_contexts_${credential}_${key}`);
+        logger.info(`Replaced ${oauthKey} with value from ${key} of IMS context ${credential}`);
       }
-      logger.debug(`${oauthKey} is in sync with AIO_ims_contexts_${credential}_${key}`);
+      logger.debug(`${oauthKey} is in sync with ${key} of IMS context ${credential}`);
     });
 
     logger.info('OAUTH env vars synced successfully');
@@ -52,56 +55,22 @@ function main() {
 }
 
 /**
- * Parses the environment variables to find those that start with 'AIO_ims_contexts_'.
- * @param {object} envVars the environment variables object
- * @returns {Array} an array of objects containing the credential, key and value from the AIO_ims_contexts_* variables
+ * Resolves the IMS server to server context from the project workspace credentials.
+ * @returns {Promise<object> | undefined} The IMS context object containing OAuth credentials.
  */
-function parseImsContextEnvVars(envVars) {
-  return Object.entries(envVars)
-    .filter(([key]) => key.startsWith('AIO_ims_contexts_'))
-    .map(([key, value]) => ({
-      ...parseAioImsContextKey(key),
-      value,
-    }));
-}
+function resolveImsS2SContext() {
+  const [credential] =
+    (Core.Config.get('project.workspace.details.credentials') ?? [])
+      // eslint-disable-next-line camelcase
+      .filter(({ integration_type }) => integration_type === 'oauth_server_to_server')
+      .map(({ name }) => name) ?? [];
 
-/**
- * Parses the AIO_ims_contexts_* key to extract the credential and key.
- * @param {string} imsKey the environment variable key that starts with 'AIO_ims_contexts_'
- * @returns {object} an object containing the credential and key extracted from the imsKey
- */
-function parseAioImsContextKey(imsKey) {
-  const str = imsKey.replace('AIO_ims_contexts_', '');
-  const [credential, key] = splitBySingleChar(str, '_');
-  return { credential, key };
-}
-
-/**
- * Splits a string by a single character, ensuring that the character is not preceded or followed by the same character.
- * @param {string} str the string to split
- * @param {string} char the character to split by
- * @returns {Array} an array of strings split by the specified character
- */
-function splitBySingleChar(str, char) {
-  const result = [];
-  let current = '';
-  let i = 0;
-  while (i < str.length) {
-    if (
-      str[i] === char &&
-      (i === 0 || str[i - 1] !== char) && // not preceded by underscore
-      (i === str.length - 1 || str[i + 1] !== char) // not followed by underscore
-    ) {
-      result.push(current);
-      current = '';
-      i++; // skip the underscore
-    } else {
-      current += str[i];
-      i++;
-    }
+  if (!credential) {
+    logger.warn('No oauth_server_to_server credentials found in the project workspace.');
+    return;
   }
-  result.push(current);
-  return result;
+
+  return context.get(credential);
 }
 
 module.exports = { main };
